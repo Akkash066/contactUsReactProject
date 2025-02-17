de57435e7777d0ee3db4970babebd0c30d22a6de
Akkash <86909011+Akkash066@users.noreply.github.com>
2025-02-17 22:11:32 +0530

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <nav className="container">
         <div className="logo">
-            <img src="/images/contact_logo.png" />
+            <img src="/images/Frame 2 1.png" />
         </div>
         <ul>
             <li>Home</li>
